40ccb4b8de0d0e4775a6b6726d51c6493ea511e9
Nikhil Madne <nikhil.madne@perennialsys.com>
2022-11-28 09:54:00 +0530

    new commit

---
+++ src/main/java/xyz/mynt/wcbootcamp/ProductMsLabApplication.java
@@ -0,0 +1,13 @@
+package xyz.mynt.wcbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMsLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMsLabApplication.class, args);
+    }
+
+}
