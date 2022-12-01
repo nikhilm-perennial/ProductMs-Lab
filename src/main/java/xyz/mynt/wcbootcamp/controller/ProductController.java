47ca59c984f6d236237331d7fbc039fed3d3dd41
Nikhil Madne <nikhil.madne@perennialsys.com>
2022-12-01 14:43:36 +0530

    new dto

--- src/main/java/xyz/mynt/wcbootcamp/controller/ProductController.java
+++ src/main/java/xyz/mynt/wcbootcamp/controller/ProductController.java
@@ -1,37 +1,44 @@
 package xyz.mynt.wcbootcamp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import xyz.mynt.wcbootcamp.dto.ProductDTO;
-import xyz.mynt.wcbootcamp.dto.ProductsDTO;
 import xyz.mynt.wcbootcamp.entity.ProductEntity;
-import xyz.mynt.wcbootcamp.service.ProductService;
+import xyz.mynt.wcbootcamp.service.impl.ProductServiceImpl;
 
 @RestController
+@Slf4j
 @RequestMapping("products")
 public class ProductController {
 
     @Autowired
-    private ProductService productService;
+    private ProductServiceImpl productService;
 
-    @GetMapping
-    public ResponseEntity<ProductsDTO> getProducts(){
-        return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
-    }
 
     @PostMapping
     public ResponseEntity<String> addProduct(@RequestBody ProductEntity productEntity){
         productService.addProduct(productEntity);
-        return new ResponseEntity<>(HttpStatusCode.valueOf(201));
+        return new ResponseEntity<>("Product Added",HttpStatusCode.valueOf(201));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> getProductById(@PathVariable String id){
+    public ResponseEntity<Object> getProductById(@PathVariable String id) {
         ProductDTO productDTO = productService.getProductById(id);
-
         return new ResponseEntity<>(productDTO,HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getProductFromDatabase(){
+        return new ResponseEntity<>(productService.getProductFromDatabase(),HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateReservedQuantity(@PathVariable String id,
+                                                         @RequestBody ProductEntity product){
+        return new ResponseEntity<>(productService.updateQuantity(id,product),HttpStatus.OK);
     }
 }
--- src/main/java/xyz/mynt/wcbootcamp/utility/MapperUtils.java
+++ src/main/java/xyz/mynt/wcbootcamp/utility/MapperUtils.java
@@ -1,6 +1,7 @@
 package xyz.mynt.wcbootcamp.utility;
 
 import xyz.mynt.wcbootcamp.dto.ProductDTO;
+import xyz.mynt.wcbootcamp.dto.ReservedProductDTO;
 import xyz.mynt.wcbootcamp.entity.ProductEntity;
 
 import java.math.RoundingMode;
@@ -17,6 +18,17 @@ public class MapperUtils {
                 .reservedQuantity(productEntity.getReservedQuantity())
                 .available(productEntity.getAvailable())
                 .build();
+    }
+
+    public static ReservedProductDTO reservedProductDTO(ProductEntity productEntity){
+        return ReservedProductDTO.builder()
+                .name(productEntity.getName())
+                .stock(productEntity.getInStock())
+                .reservedQuantity(productEntity.getReservedQuantity())
+                .available(productEntity.getAvailable())
+                .build();
+
+
     }
 
     private MapperUtils() {}
